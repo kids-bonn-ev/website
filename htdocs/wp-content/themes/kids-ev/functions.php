beb51062bd12b07a1c7316403849928e1e3eada0
Benjamin Eberlei <kontakt@beberlei.de>
2026-07-09 21:51:48 +0200

    Implemented Phase 1

--- htdocs/wp-content/themes/kids-ev/functions.php
+++ htdocs/wp-content/themes/kids-ev/functions.php
@@ -1,10 +1,24 @@
 <?php
 
+add_action('after_setup_theme', function () {
+    add_editor_style('assets/css/global.css');
+});
+
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style(
-        'kids-ev-style',
-        get_stylesheet_uri(),
+        'kids-ev-global',
+        get_theme_file_uri('assets/css/global.css'),
         [],
         wp_get_theme()->get('Version')
     );
 });
+
+// Maybug-Schnitte für den ersten Paint vorladen (Decorative + Black, siehe Plan 6.3).
+add_action('wp_head', function () {
+    foreach (['MaybugMSDecorative', 'MaybugMSBlack'] as $font) {
+        printf(
+            '<link rel="preload" href="%s" as="font" type="font/woff2" crossorigin>' . "\n",
+            esc_url(get_theme_file_uri('assets/fonts/' . $font . '.woff2'))
+        );
+    }
+}, 1);
